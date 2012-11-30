4f569a6d0b8097fbc60aec257453c242c591a24d
Carlos Guerra <carlosgm192@gmail.com>
2012-11-29 20:44:10 -0430

    Agregado entity elector y adecuado centro con las relaciones correspondientes

---
+++ src/Orion7/CoreBundle/Entity/Elector.php
@@ -0,0 +1,19 @@
+<?php
+// src/Blogger/BlogBundle/Entity/Blog.php
+
+namespace Orion7\CoreBundle\Entity;
+
+class Elector
+{
+    protected $letra_cedula;
+    protected $numero_cedula;
+    protected $primer_apellido;
+    protected $segundo_apellido;
+    protected $primer_nombre;
+    protected $segundo_nombre;
+    protected $fecha_nacimiento;
+    protected $estado;
+    protected $municipio;
+    protected $parroquia;
+    protected $centro;
+}
